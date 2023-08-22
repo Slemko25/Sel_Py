9643be7716210a4a19d622d2065194ec53334543
Yurii Hura <yuriihura25@gmail.com>
2023-08-22 15:53:14 +0300

    deleted useless files

--- Configurations/TestData.py
+++ Configurations/TestData.py
@@ -1,4 +1,4 @@
-class TestData:
+class test_data:
     URL = "https://magento.softwaretestingboard.com"
     USERNAME = "admin12@gmail.com"
     PASSWORD = "admin12!@"
--- Tests/chrome_invoke_test.py
+++ Tests/chrome_invoke_test.py
@@ -4,11 +4,30 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
+
 driver = webdriver.Chrome(service=Service(ChromeDriverManager("").install()))
 driver.get("https://magento.softwaretestingboard.com")
+driver.maximize_window()
+driver.implicitly_wait(2)
+driver.refresh()
+driver.implicitly_wait(2)
 
+driver.find_element(By.LINK_TEXT, 'Sign In').click()
+driver.implicitly_wait(2)
+
+title = driver.title
+print(title)
+assert title == 'Customer Login'
+
+driver.find_element(By.ID, 'email').send_keys("admin12@gmail.com")
+driver.find_element(By.ID, 'pass').send_keys("admin12!@")
+driver.implicitly_wait(2)
+driver.find_element(By.ID, 'send2').click()
+
+home_page = driver.title
+
+print(home_page)
+assert home_page == 'Home Page'
 time.sleep(5)
 
-
-driver.close()
 driver.quit()
